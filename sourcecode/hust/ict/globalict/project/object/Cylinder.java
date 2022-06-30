d503c58b3861fb83475f23afdcbeffbbda8b2110
DuongDQ-194747 <102663176+DuongDQ-194747@users.noreply.github.com>
2022-06-30 11:35:49 +0700

    feat: implement object package

---
+++ sourcecode/hust/ict/globalict/project/object/Cylinder.java
@@ -0,0 +1,28 @@
+package hust.ict.globalict.project.object;
+
+import hust.ict.globalict.project.utils.Shape;
+
+public class Cylinder extends MainObject{
+	
+	private double radius;
+	
+	public Cylinder(Shape shape) {
+		super(shape);
+	}
+
+	public void calAngularAcceleration() {
+		
+	}
+	
+	public void calAngularVelocity() {
+		
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+}
--- sourcecode/hust/ict/globalict/project/object/MainObject.java
+++ sourcecode/hust/ict/globalict/project/object/MainObject.java
@@ -1,9 +1,83 @@
 package hust.ict.globalict.project.object;
 
-public class MainObject {
+import hust.ict.globalict.project.force.Force;
+import hust.ict.globalict.project.utils.Shape;
 
-	public MainObject() {
-		// TODO Auto-generated constructor stub
+public class MainObject {
+	private Shape shape;
+	private double position;
+	private double mass;
+	private double acceleration;
+	private double velocity;
+	//forces
+	private Force forces[];
+	private Force sumOfForces;
+	
+	
+	
+	public MainObject(Shape shape) {
+		this.shape = shape;
+	}
+
+
+
+	public double getMass() {
+		return mass;
+	}
+
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
+	public double getPosition() {
+		return position;
+	}
+
+	public void setPosition(double position) {
+		this.position = position;
+	}
+
+	public Shape getShape() {
+		return shape;
+	}
+
+	public void setShape(Shape shape) {
+		this.shape = shape;
+	}
+
+
+
+	public double getVelocity() {
+		return velocity;
+	}
+
+
+
+	public void setVelocity(double velocity) {
+		this.velocity = velocity;
+	}
+
+
+
+	public double getAcceleration() {
+		return acceleration;
+	}
+
+
+
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+
+
+
+	public Force getSumOfForces() {
+		return sumOfForces;
+	}
+
+
+
+	public void calSumOfForces() {
 	}
 
 }
